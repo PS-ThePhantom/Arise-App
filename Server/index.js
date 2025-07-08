cdb4c7db5a69bc90aed52f29408a8867fec9efc0
Phuluso <119367220+PS-ThePhantom@users.noreply.github.com>
2025-07-08 17:17:55 +0000

    fix: index.js code syntax fix

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // API routes should come before static file serving
-a
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
